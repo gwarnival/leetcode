a2c9e50cb07fdb1eb8018ea175fc148189d570b4
Kenny Kim <kkh900629@gmail.com>
2022-03-10 22:17:28 +0900

    Time: 6 ms (99.98%), Space: 41.5 MB (76.19%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean isPalindrome(int x) {        
-        
         int origin = x;
         int palindrome = 0;
         int reminder;
@@ -8,8 +7,7 @@ class Solution {
             reminder = x % 10;
             x /= 10;
             palindrome = palindrome * 10 + reminder;
-        }
-        
+        }        
         return palindrome == origin;
     }
 }
