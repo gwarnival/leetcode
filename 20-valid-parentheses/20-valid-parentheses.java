1135b1bf732e1ab5db50f01b1a34bdc1d67559df
Kenny Kim <kkh900629@gmail.com>
2022-03-15 17:16:05 +0900

    Time: 5 ms (13.75%), Space: 42.1 MB (40.43%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put('}', '{');
+        map.put(')', '(');
+        map.put(']', '[');
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (stack.isEmpty() || stack.pop() != map.get(c)) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
