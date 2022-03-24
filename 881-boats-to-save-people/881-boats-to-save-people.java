ad4c068b041cccf84b3ebc2374b8dc1b2b186ca8
Kenny Kim <kkh900629@gmail.com>
2022-03-24 10:41:09 +0900

    Time: 24 ms (26.37%), Space: 65.8 MB (19.50%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.java
+++ 881-boats-to-save-people/881-boats-to-save-people.java
@@ -5,8 +5,7 @@ class Solution {
         int left = 0;
         int right = people.length - 1;
         while (left <= right) {
-            int sum = people[left] + people[right];
-            if (limit >= sum) {
+            if (limit >= people[left] + people[right]) {
                 left++;
             }
             right--;
