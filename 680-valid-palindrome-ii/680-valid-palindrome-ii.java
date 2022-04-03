fe7f5e4e47da01fe50a8d332a8238bddd30c7e7a
Kenny Kim <kkh900629@gmail.com>
2022-04-03 14:40:30 +0900

    Time: 10 ms (60.02%), Space: 54.2 MB (67.75%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            } else {
+                return validPalindrome(s, left, right - 1) || validPalindrome(s, left + 1, right);
+            }
+        }
+        return true;
+    }
+    
+    private boolean validPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
